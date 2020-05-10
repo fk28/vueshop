695ab531144ac6c0df30cd4bd53a72acee0706ac
fk28 <136233500@qq.com>
2020-05-11 00:28:45 +0800

    login1

---
+++ src/components/Login.vue
@@ -0,0 +1,77 @@
+<template>
+    <div class="login_container">
+        <div class="login_box">
+            <div class="avatar_box">
+                <img src="../assets/logo.png" alt="">
+            </div>
+            
+                <el-form label-width="0px" class="login_form">
+                    <el-form-item>
+                        <el-input prefix-icon="el-icon-search"></el-input>
+                    </el-form-item>
+                    <el-form-item>
+                        <el-input prefix-icon="el-icon-search"></el-input>
+                    </el-form-item>
+                    <el-form-item class="btns">
+                        <el-button type="primary">登陆</el-button>
+                        <el-button type="info">重置</el-button>
+                    </el-form-item>
+                    
+                </el-form>
+            
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="less" scoped>
+.login_container{
+    background-color: #2b4b6b;
+    height: 100%;
+}
+.login_box{
+    width: 450px;
+    height: 300px;
+    background-color: white;
+    border-radius: 3px;
+    position: absolute;
+    left: 50%;
+    top:50%;
+    transform: translate(-50%,-50%);
+    .avatar_box{
+        height: 130px;
+        width: 130px;
+        border: 1px solid #eeeeee;
+        border-radius: 50%;
+        padding: 10px;
+        box-shadow: 0 0 10px #dddddd;
+        position: absolute;
+        left: 50%;
+        transform: translate(-50%,-50%);
+        background-color: #ffffff;
+        img{
+            width:100%;
+            height: 100%;
+            border-radius: 50%;
+            background-color: #eeeeee;
+        }
+        
+    }
+    .login_form{
+         position: absolute;
+         bottom: 0;
+         width: 100%;   
+         padding: 0 20px;
+         box-sizing: border-box;
+        }
+    .btns{
+        display: flex;
+        justify-content: flex-end;
+    }
+}
+</style>
---
+++ src/components/Readme.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="readme_container">
+        Readme
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="less" scoped>
+.readme_container{
+    background-color: #2b4b6b;
+    height: 100%;
+    }
+</style>
